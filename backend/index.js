81d20668ded09ec7c23232b46248480a3c100726
Brenden Cheung <cheungbrenden@gmail.com>
2023-12-17 22:08:01 -0800

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
 
 const app = express()
 
@@ -60,38 +59,69 @@ app.get('/api/gamedata', (request, response) => {
 
 })
 
-app.get('/api/gamedata/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const game = gameData.find(game => game.id === id)
+app.get('/api/gamedata/:id', (request, response, next) => {
+    // const id = Number(request.params.id)
+    // const game = gameData.find(game => game.id === id)
 
-    if (game) {
-        response.json(game)
-    }
-    else {
-        response.status(404).end()
-    }
+    Game.findById(request.params.id).then(game => {
+        if (game) {
+            response.json(game)
+        } else {
+            response.status(404).end()
+        }
+    })
+        .catch(error => {
+            next(error)
+        })
 })
 
-app.delete('/api/gamedata/:id', (request, response) => {
-    const id = Number(request.params.id)
-    gameData = gameData.filter(game => game.id !== id)
+app.delete('/api/gamedata/:id', (request, response, next) => {
 
-    response.status(204).end()
+    Game.findByIdAndDelete(request.params.id).then(game => {
+        response.status(204).end()
+    })
+        .catch(error => next(error))
+
+
 })
 
 app.post('/api/gamedata', (request, response) => {
-    
-    const game = request.body
-    
 
+    const body = request.body
 
-    // gameData = gameData.concat(game)
-    // response.json(game)
+    // if (game.content === undefined) {
+    //     return response.status(400).json({ error: 'content missing' })
+    // }
+
+    console.log(body)
+
+    const game = new Game({
+        gameTitle: body.gameTitle,
+        score: body.score,
+        progress: body.progress
+    })
+
+    game.save().then(savedGame => {
+        response.json(savedGame)
+    })
 })
 
-// app.put('/api/gamedata/:id', (request, response) => {
+app.put('/api/gamedata/:id', (request, response, next) => {
+    const body = request.body
 
-// })
+    const game = {
+        gameTitle: body.gameTitle,
+        score: body.score,
+        progress: body.progress
+    }
+
+    Game.findByIdAndUpdate(request.params.id, game, { new: true })
+        .then(updatedGame => {
+            response.json(updatedGame)
+        })
+        .catch(error => next(error))
+
+})
 
 // need to add put request for updating a speicfic game
 
@@ -100,6 +130,19 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
